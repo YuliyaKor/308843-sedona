62eaa042bff8afdc042254d330dfdc22cb204c41
YuliyaKor <kiara_lia@mail.ru>
2018-04-02 00:54:33 +0300

    Внесены исправления

--- js/script.js
+++ js/script.js
@@ -2,19 +2,18 @@
 (function () {
   var hotelSearchButton = document.querySelector('.hotel-search-button');
   var searchForm = document.querySelector('.search-form');
-
-
-  hotelSearchButton.addEventListener('click', function () {
-    searchForm.classList.toggle('visually-hidden');
-    searchForm.classList.toggle('visually');
-  });
-
   var form = searchForm.querySelector('.hotel-search-form');
   var dateStart = form.querySelector('[name=start]');
   var dateFinish = form.querySelector('[name=finish]');
   var adults = form.querySelector('[name=adults]');
   var children = form.querySelector('[name=children]');
   var button = searchForm.querySelector('.hotel-search-form-button');
+
+  hotelSearchButton.addEventListener('click', function () {
+    searchForm.classList.toggle('visually-hidden');
+    searchForm.classList.toggle('visually');
+  });
+
   form.addEventListener("submit", function (evt) {
     if (!dateStart.value || !dateFinish.value || !adults.value || !children.value) {
       evt.preventDefault();
